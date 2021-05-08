609f2c07fe7c16b9743752fd17b9f75d195cbbe2
Eric Raymund Rex <58367989+ericraymundrex@users.noreply.github.com>
2021-05-08 12:39:58 +0530

    make a photo to upload

--- Server_upload.js
+++ Server_upload.js
@@ -7,8 +7,23 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const storage=multer.diskStorage({
+    destination:(req,file,callback)=>{
+        callback(null,'uploads');
+    },
+    filename:(req,file,callback)=>{
+        const {originalname}= file; 
+        callback(null,originalname);
+    }
+})
+const upload=multer({storage})
+
 app.get("/",(req,res)=>{
     res.render("index");
+})
+
+app.post("/upload",upload.single('userSentfile'),(req,res)=>{
+    res.send("Done");
 })
 
 app.listen(3000, () => {
